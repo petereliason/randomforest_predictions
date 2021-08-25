4f8913db0d588e89ec61657cf4c8b638b0947498
Peter Eliason <peter@petereliason.com>
2021-08-25 02:11:38 -0500

    Update train.py

--- train.py
+++ train.py
@@ -1,35 +1,20 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
-
 
 import pandas as pd
 import numpy as np
-
 import os
-# In[4]:
+
 
 print(os.getcwd())
 
-
 dataset = pd.read_csv('trainer/pima-indians-diabetes.csv')
-
-
-# In[5]:
-
-
 dataset.head()
-
-
-# In[6]:
 
 
 X = dataset.iloc[:, 0:8].values
 y = dataset.iloc[:, 8].values
-
-
-# In[8]:
 
 
 from sklearn.model_selection import train_test_split
@@ -44,17 +29,11 @@ X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
 
-# In[15]:
-
-
 from sklearn.ensemble import RandomForestClassifier
 
 clf = RandomForestClassifier(n_estimators=100)
 clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test)
-
-
-# In[16]:
 
 
 #Import scikit-learn metrics module for accuracy calculation
@@ -63,13 +42,8 @@ from sklearn import metrics
 print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
 
 
-# In[17]:
 
-
-BUCKET = 'gs://peter-test-diabetes'
-
-
-# In[18]:
+# export to joblib file. model is assigned to clf variable
 
 import joblib
 joblib.dump(clf, 'model/random_forest.joblib')
